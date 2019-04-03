14ba59a71c6054a96ee9a260ced016e7b7461b60
Emily Zou <emily@Emilys-MacBook-Pro.local>
2019-04-03 18:39:20 -0400

    arrow function practice

--- session_3/github.js
+++ session_3/github.js
@@ -2,30 +2,45 @@
 Exercise for Countdown C session 3. See git_exercise.txt for details
 */
 
-const personOne = favoriteFoods => {
-  /*insert functionality here*/
+const personOne = favoriteFoods => { // define opening and closing of the function
+  // for (let i = 0; i < favoriteFoods.length; i++) {
+  //   console.log(i);
+  // }
+  favoriteFoods.map(
+    food => (console.log(food)) // food represents each element in array; this food is a parameter
+  )
 };
 
 const personTwo = favoriteMovies => {
-  /*insert functionality here*/
+  // for (let i = 0; i < favoriteMovies.length; i++) {
+  //   console.log(i);
+  // }
+  favoriteMovies.map(
+    movie => (console.log(movie)) // food represents each element in array; this food is a parameter
+  )
 };
 
 /*you can opt out of this third function if you only have two partners*/
 const personThree = favoriteAnimals => {
-  /*insert functionality here*/
+  // for (let i = 0; i < favoriteAnimals.length; i++) {
+  //   console.log(i);
+  // }
+  favoriteAnimals.map(
+    animal => (console.log(animal)) // food represents each element in array; this food is a parameter
+  )
 };
 
 /*
 What does this variable syntax do?
 */
-const favoriteFoods = [],
-  favoriteMovies = [],
-  favoriteAnimals = [];
+const favoriteFoods = ['pizza', 'Roots bowls', 'noodles'],
+  favoriteMovies = ['Little Miss Sunshine', 'Harry Potter'],
+  favoriteAnimals = ['penguins', 'wolf', 'bunny'];
 
 const iceBreaker = () => {
-  /*
-    Call each function here
-    */
+  personOne
+  personTwo
+  personThree
 };
 
 /*this calls iceBreaker, which will call all of your functions*/
